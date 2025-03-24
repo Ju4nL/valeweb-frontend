135ecbbf5299eb266e3607fbbcd7be83eb8023ac
Juan <juanlozadacochaches@gmail.com>
2025-03-24 16:11:31 -0500

    update redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/', // ✅ esto es clave para que Render sirva correctamente las rutas
   plugins: [
+    react(),
     tailwindcss(),
   ],
-})
+});
